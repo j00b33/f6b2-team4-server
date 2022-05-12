ec3a272c6335040e1fcdb4c194b017723692af88
j00b33 <pjoobee@gmail.com>
2022-05-12 22:26:16 +0900

    ERROR

--- src/apis/user/dto/updateUser.input.ts
+++
@@ -1,5 +0,0 @@
-import { InputType, PartialType } from '@nestjs/graphql';
-import { CreateUserInput } from './user.input';
-
-@InputType()
-export class UpdateUserInput extends PartialType(CreateUserInput) {} //전체를 들고와도 되고 안들고 와도 되고
