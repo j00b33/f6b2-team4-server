ceea467a71a6eca81d68b3b9d732e80943afda23
hiosi123 <s@Sui-MacBookAir.local>
2022-05-13 19:43:09 +0900

    Update - feature#46

--- src/commons/auth/jwt-refresh.strategy.ts
+++ src/commons/auth/jwt-refresh.strategy.ts
@@ -1,10 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import {
+  CACHE_MANAGER,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'refresh') {
-  constructor() {
+  constructor(
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
+  ) {
     super({
       //검중부, Bearer 뺴고 넣어야함, 내장 되어있음 .fromauthheadera
       jwtFromRequest: (req) => {
@@ -18,13 +27,22 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'refresh') {
         return result;
       },
       secretOrKey: 'myRefreshKey', //이부분이 다다름
+      passReqToCallback: true,
     });
   }
 
-  validate(payload) {
+  async validate(req, payload) {
     // 실패 또는 성공여부를 알려줌
     //검증완료되면 실행
-    console.log(payload);
+    const bakeCookie = req.headers.cookie.split(' ');
+    const result = bakeCookie[bakeCookie.length - 1].replace(
+      'refreshToken=',
+      '',
+    ); //
+    const check = await this.cacheManager.get(`refreshToken: ${result}`);
+    //검증완료되면 실행
+    if (check) throw new UnauthorizedException();
+
     return {
       id: payload.sub, //리턴이 된거임 context 라는 곳으로
       email: payload.email,
