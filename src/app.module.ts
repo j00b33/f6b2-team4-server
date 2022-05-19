7fc550f2f1f30e8bf03301adb1884d3a7edb9105
j00b33 <pjoobee@gmail.com>
2022-05-19 14:38:08 +0900

    ERROR

--- src/app.module.ts
+++ src/app.module.ts
@@ -44,7 +44,7 @@ import { ReceiptModule } from './apis/receipt/receipt.module';
       type: 'mysql',
       // host: '10.127.112.4', //team04backend.shop
       host: '172.27.48.3', //hiosi123.shop
-      // host: 'my-database',
+      // host: 'my-database', // localhost
       port: 3306,
       username: 'root',
       password: 'root',
@@ -55,7 +55,7 @@ import { ReceiptModule } from './apis/receipt/receipt.module';
     }),
     CacheModule.register<RedisClientOptions>({
       store: redisStore,
-      // url: 'redis://my-redis:6379',
+      // url: 'redis://my-redis:6379', // localhost
       // url: 'redis://:oj4tpyWX@10.140.0.3:6379', //team04backend.shop
       url: 'redis://:NvUF5Lwg@10.140.0.2:6379', //hiosi123.shop
       isGlobal: true,
