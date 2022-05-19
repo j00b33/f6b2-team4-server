f8cd4e548caadcd779c89e227620a9cfaff3298a
j00b33 <pjoobee@gmail.com>
2022-05-19 21:46:24 +0900

    Create - feature#92

--- src/apis/receipt/receipt.resolver.ts
+++ src/apis/receipt/receipt.resolver.ts
@@ -1,5 +1,5 @@
 import { UseGuards } from '@nestjs/common';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { GqlAuthAccessGuard } from 'src/commons/auth/gql-auth.guard';
 import { CurrentUser, ICurrentUser } from 'src/commons/auth/gql-user.param';
 import { Receipt } from './entities/receipt.entity';
@@ -8,6 +8,12 @@ import { ReceiptService } from './receipt.service';
 @Resolver()
 export class ReceiptResolver {
   constructor(private readonly receiptService: ReceiptService) {}
+
+  @UseGuards(GqlAuthAccessGuard)
+  @Query(() => [Receipt])
+  async fetchReceipts(@CurrentUser() currentUser: ICurrentUser) {
+    return await this.receiptService.findAll({ currentUser });
+  }
 
   @UseGuards(GqlAuthAccessGuard)
   @Mutation(() => Receipt)
